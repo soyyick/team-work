a2677f33da1e6818057dd60e1cd926b34e98b389
Theerapong2004 <148409276+Theerapong2004@users.noreply.github.com>
2024-09-09 20:38:06 +0700

    งานข่อย

--- team_noob/urls.py
+++ team_noob/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 urlpatterns = [
     path("", views.indexPage, name= 'index'),
+<<<<<<< HEAD
     path('continue/', views.continue_view, name='continue'),
+=======
+    path("login/", views.loginPage, name= 'login'),
+    path("map/", views.mappage, name= 'map'),
+>>>>>>> e09930b19ee8f0f38869578d4db7dbdeecd94b04
 ]
--- team_noob/views.py
+++ team_noob/views.py
@@ -6,5 +6,22 @@ from django.shortcuts import render
 # Create your views here.
 def indexPage(request):
     return render(request, 'index.html')
+<<<<<<< HEAD
 def continue_view(request):
     return render(request, 'continue.html')
+=======
+def loginPage(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')  # เปลี่ยน 'home' เป็นชื่อ URL ของหน้าหลักที่คุณต้องการให้ผู้ใช้เข้าสู่หลังจาก login
+        else:
+            messages.error(request, 'Invalid username or password')
+    return render(request, 'login.html')
+def mappage(request):
+    return render(request, 'map.html')
+>>>>>>> e09930b19ee8f0f38869578d4db7dbdeecd94b04
